1935f81e95221b37e5a3b47457da67e696571e0a
smochan <img_2019052@iiitm.ac.in>
2021-09-13 20:29:02 +0530

    some changes

--- app.js
+++ app.js
@@ -1,37 +1,38 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
-const database = require("./database")
-const { register } = require('./controllers/auth/register');
-const { login } = require('./controllers/auth/login');
-const { temp } = require('./controllers/temp');
-const { amount } = require('./controllers/amount');
-const { group } = require('./controllers/group');
-const cookieParser = require('cookie-parser');
-
+const database = require("./database");
+const { register } = require("./controllers/auth/register");
+const { login } = require("./controllers/auth/login");
+const { temp } = require("./controllers/temp");
+const { amount } = require("./controllers/amount");
+const { group } = require("./controllers/group");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 app.use(cookieParser());
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
 
 const start = async () => {
-    await database.connect();
+  await database.connect();
 
-    app.get("/", function(req, res){
-        res.send("Hello World");
-    })
-    // app.get("/login", temp);
-    app.get("/register", temp);
-    app.get("/group", group);
+  app.get("/", function (req, res) {
+    res.send("Hello World");
+  });
+  // app.get("/login", temp);
+  app.get("/register", temp);
+  app.post("/group", group);
 
-    app.post("/register", register);
-    app.post("/payment", amount);
+  app.post("/register", register);
+  app.post("/payment", amount);
 
-    app.post("/login", login);
+  app.post("/login", login);
 
-    app.listen(3000, function(){
-        console.log("server started on port 3000")
-    })
-}
+  app.listen(3000, function () {
+    console.log("server started on port 3000");
+  });
+};
 
 start();
--- controllers/group.js
+++ controllers/group.js
@@ -1,12 +1,22 @@
 const Group = require("../models/group");
 const User = require("../models/user");
+const mongoose = require("mongoose");
 
 const group = (req, res) => {
-    const newGroup = new Group;
-    console.log(User.find({}));
-    // newGroup.member.push()
-}
+  console.log(req.body);
+  const newGroup = new Group({
+    member: req.body.member,
+    amount: req.body.amount,
+  });
+  newGroup.save(function (err) {
+    if (err) console.log(err);
+    else {
+      res.status(200).send("group added");
+      console.log("group added");
+    }
+  });
+};
 
 module.exports = {
-    group,
+  group,
 };
