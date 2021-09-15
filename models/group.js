718efe49134c7db206828b538a73950f602bbb3a
smochan <img_2019052@iiitm.ac.in>
2021-09-15 19:41:43 +0530

    corrected group schema

--- models/group.js
+++ models/group.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
 const { User } = require("./user");
-const { Amount } = require("./amount");
 
 const group = new mongoose.Schema({
-  groupName: String,
-  member: [{ type: mongoose.Schema.ObjectId, ref: User }],
-  amount: [{ type: mongoose.Schema.ObjectId, ref: Amount }],
+  gName: String,
+  members: [{ type: mongoose.Schema.ObjectId, ref: User }],
 });
 
 module.exports = mongoose.model("Group", group);
