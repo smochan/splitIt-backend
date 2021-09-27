d6ccebc600fd0b99d5fcb2bb4d983fd96cef7cdf
palakjain21 <21palak2001@gmail.com>
2021-09-27 09:09:52 +0530

    Added models of user and amount

--- controllers/amount.js
+++ controllers/amount.js
@@ -1,19 +1,19 @@
-const Amount = require("../models/amount")
+const Amount = require("../models/amount");
 
 const amount = (req, res) => {
-    console.log(typeof req.body);
+  console.log(typeof req.body);
 
-    const newAmount = new Amount({
-        amount: req.body.amount,
-        comment: req.body.comment,
-        status: req.body.status,
-    });
-    newAmount.save(function (err) {
-        if(err) console.log(err);
-        else console.log("amount added");
-    });
-}
+  const newAmount = new Amount({
+    amount: req.body.amount,
+    comment: req.body.comment,
+    status: req.body.status,
+  });
+  newAmount.save(function (err) {
+    if (err) console.log(err);
+    else console.log("amount added");
+  });
+};
 
 module.exports = {
-    amount,
+  amount,
 };
