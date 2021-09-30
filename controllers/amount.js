5ed8f3a2453103657490a1e3dab93818504ae168
palakjain21 <21palak2001@gmail.com>
2021-09-30 19:21:39 +0530

    Added amount controller

--- controllers/amount.js
+++ controllers/amount.js
@@ -1,17 +1,23 @@
 const Amount = require("../models/amount");
 
-const amount = (req, res) => {
-  console.log(typeof req.body);
+const amount = async (req, res) => {
+  try {
+    console.log(typeof req.body);
 
-  const newAmount = new Amount({
-    amount: req.body.amount,
-    comment: req.body.comment,
-    status: req.body.status,
-  });
-  newAmount.save(function (err) {
-    if (err) console.log(err);
-    else console.log("amount added");
-  });
+    const newAmount = new Amount({
+      // payer: req.body.payer,
+      amount: req.body.amount,
+      comment: req.body.comment,
+      status: req.body.status,
+      contributors: req.body.contributors,
+    });
+    await newAmount.save();
+
+    res.status(200).send("amount added");
+    console.log("amount added");
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 module.exports = {
