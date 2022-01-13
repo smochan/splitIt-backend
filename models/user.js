02f6880adc38eb3a6f71623e03456c9a024046f5
smochan <img_2019052@iiitm.ac.in>
2022-01-13 16:35:25 +0530

    abcd

--- models/user.js
+++ models/user.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
+import { Group } from "./group";
 
 const user = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
   mobile: Number,
+  // friends?: Array,
+  groups?: [{ type: mongoose.Schema.ObjectId, ref: Group}]
 });
 
 module.exports = mongoose.model("User", user);
